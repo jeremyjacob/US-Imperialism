cacc23a99d0ea8feffc19cf26cafdc227e603957
jeremyjacob <44799944+jeremyjacob@users.noreply.github.com>
2021-11-30 12:54:53 -0800

    Yeee

--- us-imperialsm/src/App.svelte
+++ us-imperialsm/src/App.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <main>
+	<h1><span>American Imperialism</span></h1>
 	<Map />
 </main>
 
@@ -12,40 +13,10 @@
 			Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 	}
 
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	img {
-		height: 16rem;
-		width: 16rem;
-	}
-
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 2rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 1rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
+		position: absolute;
+		top: -0.2em;
+		width: 100%;
+		text-align: center;
 	}
 </style>
--- us-imperialsm/src/lib/Map.svelte
+++ us-imperialsm/src/lib/Map.svelte
@@ -1,8 +1,51 @@
 <script lang="ts">
 	import map from '../assets/map.svg?raw'
+	import { tweened } from 'svelte/motion'
+	import { cubicOut } from 'svelte/easing'
+
+	let opts = {
+		duration: 500,
+		easing: cubicOut,
+	}
+
+	let scale = tweened(1, opts)
+	let x = tweened(0, opts)
+	let y = tweened(0, opts)
+
+	let mousePrev = { x: 0, y: 0 }
+	let mapPrev = { x: 0, y: 0 }
+
+	document.addEventListener('mousedown', (event) => {
+		document.addEventListener('mousemove', move)
+		mousePrev.x = event.x
+		mousePrev.y = event.y
+		mapPrev.x = $x
+		mapPrev.y = $y
+	})
+	document.addEventListener('mouseup', (event) =>
+		document.removeEventListener('mousemove', move)
+	)
+
+	function move(event: MouseEvent) {
+		const _x = mousePrev.x - event.x
+		const _y = mousePrev.y - event.y
+
+		x.set(mapPrev.x - _x * 0.8)
+		y.set(mapPrev.y - _y * 0.8)
+	}
+
+	window.addEventListener('mousewheel', (event: any) => {
+		const newScale = $scale + event.deltaY * 0.01
+		if (newScale <= 1) {
+			scale.set(1)
+			x.set(0)
+			y.set(0)
+		} else if (newScale >= 5) scale.set(5)
+		else scale.set(newScale)
+	})
 </script>
 
-<div class="container">
+<div class="container" style="--x:{$x}px; --y:{$y}px; --scale:{$scale};">
 	{@html map}
 </div>
 
@@ -15,6 +58,21 @@
 		&:hover {
 			fill: red;
 			transform: translate3d(0, -5px, 0);
+		}
+	}
+
+	:global(svg) {
+	}
+
+	.container {
+		transform: scale(var(--scale)) translate(var(--x), var(--y))
+			translate3d(0px, 1px, 0px);
+		backface-visibility: hidden;
+		margin-top: -0.4em;
+		cursor: grab;
+
+		&:active {
+			cursor: grabbing;
 		}
 	}
 </style>
