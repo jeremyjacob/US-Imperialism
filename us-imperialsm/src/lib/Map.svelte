2d2bd6c451dd837dafcfc79f27eb88a6a2b47bfb
Jeremy Jacob <github@jeremyjacob.dev>
2021-11-29 23:29:42 -0800

    Init

---
+++ us-imperialsm/src/lib/Map.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import map from '../assets/map.svg?raw'
+</script>
+
+<div class="container">
+	{@html map}
+</div>
+
+<style lang="scss">
+	:global(.region) {
+		transition: all 0.4s cubic-bezier(0.33, 1, 0.68, 1);
+		transform: translate3d(0, 0.01px, 0);
+		backface-visibility: hidden;
+
+		&:hover {
+			fill: red;
+			transform: translate3d(0, -5px, 0);
+		}
+	}
+</style>
