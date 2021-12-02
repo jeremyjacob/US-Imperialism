e7ce230ca6e82278bc1d9e4f2e5049674d571c66
Jeremy Jacob <github@jeremyjacob.dev>
2021-12-01 23:27:46 -0800

    Last

---
+++ src/lib/data.ts
@@ -0,0 +1,59 @@
+export default {
+	china: {
+		coords: [370, 325],
+		height: 336,
+		country: 'China',
+		body: `The Chinese trade market has long been one of the most important in the whole world, and Americans were not blind to this. The United states could see how crucial it was to be a part of Chinese trade, so, naturally, they wanted in. The US Secretary of State, John Hay, decided to write a letter to the major European powers. The letter stated Hay’s opinion that China should be kept open to trade with all other countries equally, and that no one country should hold complete control over their exports.
+		This letter was met with, well, not much. It didn’t really have any legal standing, so said major European powers weren’t very inclined to listen.`,
+	},
+	natives: {
+		coords: [505, 302],
+		height: 372,
+		country: 'Western Natives',
+		body: `Native lands were taken through force while their inhabitants were denied citizenship and exiled to reservations. The United States sustained this aggression and eventually the Military ended up at war with the Lakota people. During this war, US Colonel, George Custer, planned to force the tribe into a reservation, but he greatly underestimated the strength and willpower of the natives. During what would be referred to as “Custer’s Last Stand,” the military was greatly outnumbered and 200 men were killed. The military struck back and arrived on Wounded Knee Creek and demanded the Lakota people to hand over their guns. When they refused, the Military slaughtered 200 unarmed native men, women, and children.`,
+	},
+	alaska: {
+		coords: [376, 104],
+		height: 335,
+		country: 'Alaska',
+		body: `When President Andrew Johnson made the decision to purchase Alaska from the Russians, many Americans thought he was an idiot. As far as anyone knew, the land was completely useless and barren, but Johnson saw something in the frozen landscape. He wanted to establish Alaska as a military base and as a hub for trade with Asian countries. Russia was eager to sell the land in order to throw off their greatest enemy, the British. Johnson purchased the land for $7.2 million. 
+As it turned out, Alaska is rich in natural resources. Americans eventually found large deposits of oil, gas, and, our personal favorite, gold.`,
+	},
+	hawaii: {
+		coords: [340, 219],
+		height: 516,
+		country: 'Hawaii',
+		body: `American’s had long had their eyes set on the succulent and juicy little island chain tucked in the middle of the vast Pacific. It has an excellent natural Harbor, which is perfect for a military base, and a warm tropical climate perfect for growing bananas and pineapples. American farmers made their way to the islands in an effort to exploit the warm climate and grow their agriculture. 
+
+		The US made an agreement with the Hawaiians. In exchange for allowing the US military to use Pearl Harbor, sugar imports from Hawaii were exempt from taxes. However, when President Mckinley passed the Mckinley Tariff, he replaced the tax on Hawaiian Sugar. American Farmers living on the Islands were outraged. Sanford Dole led the rest of the American planters in an effort to overthrow the Hawaiian Queen. After queen Liliuokalani was taken out of power, the farmers continued to push for the US to annex the islands so no tariffs would apply to the imports. President Cleveland said he would not do so unless the majority of Hawiians wanted it. However, when Mckinley was elected after, he decided to take over the islands anyways.`,
+	},
+	cuba: {
+		coords: [680, 260],
+		height: 476,
+		country: 'Cuba',
+		body: `Cuba, like Hawaii, was also quite enticing for Americans. They have a similarly warm climate, allowing them to grow crops like sugarcane, and the close proximity to Florida makes it ideal as a trade hub. The US also saw the island as an opportune place for a naval base. However, unlike Hawaii, Cuba was in the middle of a war for their own independence from Spain. Because the US got tons of trade from Cuba, they figured that Cuba losing the war would not be particularly good for business.
+		The US sends the USS Maine to Havana Harbor to help “protect” Cubans. The ship mysteriously explodes, and the US declares war on Spain. Despite the US going to war in order to help Cuba, the Teller Amendment was passed to say that the US would not try to take over the island. A few months later, the US has control of Cuba, and negotiations for the end of the Spanish-American War begin. The war was finally ended by the Treaty of Paris. The US pays Spain $20 million in exchange for Guam, the Philippines, and Puerto Rico.`,
+	},
+	philippines: {
+		coords: [480, 80],
+		height: 396,
+		country: 'Philippines',
+		body: `The Philippines were also under Spanish control at the time when the US was allowing itself to be swept off its feet by any and every country that came its way. The Philippines were just America's type with their tropical climate, perfect location near Asia and islandy physique. Then the Spanish-American war happens, and America buys the Philippines. 
+		Immediately after the war ends, fighting breaks out between Filipino citizens and American forces. The ensuing Filipino-American War ended with 4,200 Americans and 200,000 Filipinos dead. Author Rudyard Kipling Writes “White man’s Burden” criticizing the US idea that it is our duty to “Civilize” other cultures, one of the root ideas of imperialism and a main driver of America's intense savior complex.`,
+	},
+	panama: {
+		coords: [680, 308],
+		height: 416,
+		country: 'Panama',
+		body: `Despite the publishing of Ruthyard Kipling’s “White Man’s Burden,” the general idea which he had criticized still remained in many Americans, and it just so happened that another country was in the midst of a struggle for independence from Columbia. Conveniently, this country, Panama, was also home to a ten mile stretch of land where the French had already tried, and failed, to build a canal. This canal was incredibly important because it would allow for Americans to make more money faster. It would cut down their travel time, and thus travel costs, vastly, and it would allow the US to have more of a presence in the Western Hemisphere.
+
+		The US military armed Panamanian rebels and helped them to win their independence. After they had won, Americans picked up where the French left off and completed construction on the canal`,
+	},
+	honduras: {
+		coords: [663, 368],
+		height: 236,
+		country: 'Honduras & Nicaragua',
+		body: `Americans wanted to protect the tobacco, sugar, and banana imports that were coming from Honduras, and they feared that some foreign power would take Nicaragua and try to establish themselves on our turf.
+		Americans decided the best course of action was to maintain a constant presence in Nicaragua for many years in order to “protect them.”`,
+	},
+}
